cd4cf862344b9ddaeab38c36c72e49aa114ef2f1
shimoshida <moshimoshimoshida@gmail.com>
2025-09-20 15:50:26 +0900

    add Random Line Space Pattern #7

--- src/mask_pattern.py
+++ src/mask_pattern.py
@@ -7,17 +7,12 @@ class MaskPattern(ABC):
     """Abstract base class for mask pattern generation"""
 
     @abstractmethod
-    def maskgen(self, ndivx: int, ndivy: int) -> np.ndarray:
-        """Generate 2D mask pattern
+    def _generate(self, mask_w: int, mask_h: int) -> np.ndarray:
+        raise NotImplementedError("Subclasses must implement this method")
 
-        Args:
-            ndivx: X dimension size
-            ndivy: Y dimension size
-
-        Returns:
-            2D numpy array representing the mask
-        """
-        pass
+    @abstractmethod
+    def __call__(self, mask_w: int, mask_h: int) -> np.ndarray:
+        raise NotImplementedError("Subclasses must implement this method")
 
 
 class LinePattern(MaskPattern):
@@ -55,37 +50,35 @@ class LinePattern(MaskPattern):
         for i in range(sum_val, n_pixels):
             if line == 0:
                 mask1d[i] = 1
-            else:
-                mask1d[i] = 0
 
         return mask1d
 
-    def __generate(self, ndivx: int, ndivy: int) -> np.ndarray:
-        mask2d = np.zeros((ndivy, ndivx), dtype=int)
+    def _generate(self, mask_w: int, mask_h: int) -> np.ndarray:
+        mask2d = np.zeros((mask_h, mask_w), dtype=int)
         space = random.randint(0, 1)
         sum_val = 0
 
-        while sum_val < ndivx - self.cd:
+        while sum_val < mask_w - self.cd:
             if space == 1:
                 space = 0
             else:
-                mask1dy = self.rand_mask_line(ndivy, gap=self.gap)
-                for i in range(sum_val, min(sum_val + self.cd, ndivx)):
-                    for j in range(ndivy):
+                mask1dy = self.rand_mask_line(mask_h, gap=self.gap)
+                for i in range(sum_val, min(sum_val + self.cd, mask_w)):
+                    for j in range(mask_h):
                         mask2d[j, i] = mask1dy[j]
                 space = 1
             # update
             sum_val += self.cd
 
-        for i in range(sum_val, ndivx):
+        for i in range(sum_val, mask_w):
             if space == 1:
-                for j in range(ndivy):
+                for j in range(mask_h):
                     mask2d[i, j] = mask1dy[j]
 
         return mask2d
 
-    def maskgen(self, ndivx: int, ndivy: int) -> np.ndarray:
-        mask = self.__generate(ndivx, ndivy)
+    def __call__(self, mask_w: int, mask_h: int) -> np.ndarray:
+        mask = self._generate(mask_w, mask_h)
         if self.direction == "H":
             mask = mask.T
 
@@ -95,83 +88,29 @@ class LinePattern(MaskPattern):
         return mask
 
 
-class LSDFPattern(MaskPattern):
-    """Line Space Dark Field pattern"""
+class RandomLineSpacePattern(MaskPattern):
 
-    def __init__(self, cd: int = 60, fac: float = 1.0, fac1d: float = 3.0):
+    def __init__(
+        self,
+        cd: int = 60,
+        fac: float = 1.0,
+        fac1d: float = 3.0,
+        field_type: str = "BF",
+    ):
         self.cd = cd
         self.fac = fac
         self.fac1d = fac1d
+        self.field_type = field_type
 
-    def maskgen(self, ndivx: int, ndivy: int) -> np.ndarray:
-        mask2d = np.zeros((ndivy, ndivx), dtype=int)
-        mask1dx = np.zeros(ndivx, dtype=int)
-        mask1dy = np.zeros(ndivy, dtype=int)
-
-        # Y direction processing
-        space = random.randint(0, 1)
-        sum_val = 0
-
-        while sum_val < ndivy - self.cd:
-            a = int(self.cd * np.exp(self.fac * random.random()))
-            if space == 1:
-                for i in range(sum_val, min(sum_val + a, ndivy)):
-                    for j in range(ndivx):
-                        mask2d[i, j] = 1
-                space = 0
-            else:
-                mask1dx = self.randmask(ndivx, cd=self.cd, fac1d=self.fac1d)
-                for i in range(sum_val, min(sum_val + a, ndivy)):
-                    for j in range(ndivx):
-                        mask2d[i, j] = mask1dx[j]
-                space = 1
-            sum_val += a
-
-        for i in range(sum_val, ndivy):
-            if space == 0:
-                for j in range(ndivx):
-                    mask2d[i, j] = 1
-            else:
-                for j in range(ndivx):
-                    mask2d[i, j] = mask1dx[j]
-
-        # X direction processing
-        space = random.randint(0, 1)
-        sum_val = 0
-
-        while sum_val < ndivx - self.cd:
-            a = int(self.cd * np.exp(self.fac * random.random()))
-            if space == 1:
-                space = 0
-            else:
-                mask1dy = self.randmask(ndivy, cd=self.cd, fac1d=self.fac1d)
-                for i in range(sum_val, min(sum_val + a, ndivx)):
-                    for j in range(ndivy):
-                        mask2d[j, i] = min(mask1dy[j], mask2d[j, i])
-                space = 1
-            sum_val += a
-
-        for i in range(sum_val, ndivx):
-            if space == 0:
-                space = 1
-            else:
-                for j in range(ndivy):
-                    mask2d[j, i] = min(mask1dy[j], mask2d[j, i])
-
-        return mask2d
-
-    def randmask(self, ndiv: int, **kwargs) -> np.ndarray:
-        cd = kwargs.get("cd", self.cd)
-        fac1d = kwargs.get("fac1d", self.fac1d)
-        mask1d = np.zeros(ndiv, dtype=int)
-
+    def randmask(self, mask_edge_len: int) -> np.ndarray:
+        mask1d = np.zeros(mask_edge_len, dtype=int)
         line = random.randint(0, 1)
         sum_val = 0
 
-        while sum_val < ndiv - cd:
-            a = int(cd * np.exp(fac1d * random.random()))
+        while sum_val < mask_edge_len - self.cd:
+            a = int(self.cd * np.exp(self.fac1d * random.random()))
 
-            for i in range(sum_val, min(sum_val + a, ndiv)):
+            for i in range(sum_val, min(sum_val + a, mask_edge_len)):
                 if line == 1:
                     mask1d[i] = 0
                 else:
@@ -180,130 +119,72 @@ class LSDFPattern(MaskPattern):
             line = 1 - line
             sum_val += a
 
-        for i in range(sum_val, ndiv):
+        for i in range(sum_val, mask_edge_len):
             if line == 0:
                 mask1d[i] = 0
             else:
                 mask1d[i] = 1
-
         return mask1d
 
-
-class LSBFPattern(MaskPattern):
-    """Line Space Bright Field pattern"""
-
-    def __init__(self, cd: int = 60, fac: float = 1.0, fac1d: float = 3.0):
-        self.cd = cd
-        self.fac = fac
-        self.fac1d = fac1d
-
-    def maskgen(self, ndivx: int, ndivy: int) -> np.ndarray:
-        mask2d = np.zeros((ndivy, ndivx), dtype=int)
-        mask1dx = np.zeros(ndivx, dtype=int)
-        mask1dy = np.zeros(ndivy, dtype=int)
+    def _generate(self, mask_w: int, mask_h: int) -> np.ndarray:
+        mask2d = np.zeros((mask_h, mask_w), dtype=int)
+        mask1dx = np.zeros(mask_w, dtype=int)
+        mask1dy = np.zeros(mask_h, dtype=int)
 
         # Y direction processing
         space = random.randint(0, 1)
         sum_val = 0
 
-        while sum_val < ndivy - self.cd:
+        while sum_val < mask_h - self.cd:
             a = int(self.cd * np.exp(self.fac * random.random()))
             if space == 1:
-                for i in range(sum_val, min(sum_val + a, ndivy)):
-                    for j in range(ndivx):
-                        mask2d[i, j] = 0
+                for i in range(sum_val, min(sum_val + a, mask_h)):
+                    for j in range(mask_w):
+                        mask2d[j, i] = 1
                 space = 0
             else:
-                mask1dx = self.randmask(ndivx, cd=self.cd, fac1d=self.fac1d)
-                for i in range(sum_val, min(sum_val + a, ndivy)):
-                    for j in range(ndivx):
-                        mask2d[i, j] = mask1dx[j]
+                mask1dx = self.randmask(mask_w)
+                for i in range(sum_val, min(sum_val + a, mask_h)):
+                    for j in range(mask_w):
+                        mask2d[j, i] = mask1dx[j]
                 space = 1
             sum_val += a
 
-        for i in range(sum_val, ndivy):
+        for i in range(sum_val, mask_h):
             if space == 0:
-                for j in range(ndivx):
-                    mask2d[i, j] = 0
+                for j in range(mask_w):
+                    mask2d[j, i] = 1
             else:
-                for j in range(ndivx):
-                    mask2d[i, j] = mask1dx[j]
+                for j in range(mask_w):
+                    mask2d[j, i] = mask1dx[j]
 
         # X direction processing
         space = random.randint(0, 1)
         sum_val = 0
 
-        while sum_val < ndivx - self.cd:
+        while sum_val < mask_w - self.cd:
             a = int(self.cd * np.exp(self.fac * random.random()))
             if space == 1:
                 space = 0
             else:
-                mask1dy = self.randmask(ndivy, cd=self.cd, fac1d=self.fac1d)
-                for i in range(sum_val, min(sum_val + a, ndivx)):
-                    for j in range(ndivy):
-                        mask2d[j, i] = max(mask1dy[j], mask2d[j, i])
+                mask1dy = self.randmask(mask_h)
+                for i in range(sum_val, min(sum_val + a, mask_w)):
+                    for j in range(mask_h):
+                        mask2d[i, j] = min(mask1dy[j], mask2d[i, j])
                 space = 1
             sum_val += a
 
-        for i in range(sum_val, ndivx):
+        for i in range(sum_val, mask_w):
             if space == 0:
                 space = 1
             else:
-                for j in range(ndivy):
-                    mask2d[j, i] = max(mask1dy[j], mask2d[j, i])
-
+                for j in range(mask_h):
+                    mask2d[i, j] = min(mask1dy[j], mask2d[i, j])
         return mask2d
 
-    def randmask(self, ndiv: int, **kwargs) -> np.ndarray:
-        cd = kwargs.get("cd", self.cd)
-        fac1d = kwargs.get("fac1d", self.fac1d)
-        mask1d = np.zeros(ndiv, dtype=int)
+    def __call__(self, mask_w: int, mask_h: int) -> np.ndarray:
+        mask = self._generate(mask_w, mask_h)
+        if self.field_type == "DF":
+            mask = 1 - mask
 
-        line = random.randint(0, 1)
-        sum_val = 0
-
-        while sum_val < ndiv - cd:
-            a = int(cd * np.exp(fac1d * random.random()))
-
-            for i in range(sum_val, min(sum_val + a, ndiv)):
-                if line == 1:
-                    mask1d[i] = 1
-                else:
-                    mask1d[i] = 0
-
-            line = 1 - line
-            sum_val += a
-
-        for i in range(sum_val, ndiv):
-            if line == 0:
-                mask1d[i] = 1
-            else:
-                mask1d[i] = 0
-
-        return mask1d
-
-
-# Pattern factory function
-# def create_mask_pattern(pattern_type: str, **kwargs) -> MaskPattern:
-#     """Factory function to create mask pattern instances
-
-#     Args:
-#         pattern_type: Type of pattern ('HBF', 'VDF', 'HDF', 'VBF', 'LSDF', 'LSBF')
-#         **kwargs: Pattern-specific parameters
-
-#     Returns:
-#         MaskPattern instance
-#     """
-#     patterns = {
-#         "HBF": HBFPattern,
-#         "VDF": VDFPattern,
-#         "HDF": HDFPattern,
-#         "VBF": VBFPattern,
-#         "LSDF": LSDFPattern,
-#         "LSBF": LSBFPattern,
-#     }
-
-#     if pattern_type not in patterns:
-#         raise ValueError(f"Unknown pattern type: {pattern_type}")
-
-#     return patterns[pattern_type](**kwargs)
+        return mask
